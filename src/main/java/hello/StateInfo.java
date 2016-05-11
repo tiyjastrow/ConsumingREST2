7f002978e0c82fa084189a533907b2957886476e
senorlewis <derrickrlewis@gmail.com>
2016-05-11 14:42:04 -0500

    Changed Value to Diff Value.

--- src/main/java/hello/StateInfo.java
+++ src/main/java/hello/StateInfo.java
@@ -5,5 +5,26 @@ package hello;
  */
 public class StateInfo {
     String type;
-    Value value;
+    DiffValue diffValue;
+
+    public StateInfo(String type, DiffValue diffValue) {
+        this.type = type;
+        this.diffValue = diffValue;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public DiffValue getDiffValue() {
+        return diffValue;
+    }
+
+    public void setDiffValue(DiffValue diffValue) {
+        this.diffValue = diffValue;
+    }
 }
