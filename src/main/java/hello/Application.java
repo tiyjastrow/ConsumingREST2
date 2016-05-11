bf5c8981469e93be94d30bcda0b71a3e3035049d
John Jastrow <jjastrow>
2016-05-11 12:02:44 -0500

    started change - not working

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -16,10 +16,29 @@ public class Application implements CommandLineRunner {
         SpringApplication.run(Application.class);
     }
 
+    /*
+    {
+  "RestResponse" : {
+    "result" : {
+      "countryIso2" : "US",
+      "stateAbbr" : "AZ",
+      "postal" : "85613",
+      "continent" : "North America",
+      "state" : "Arizona",
+      "longitude" : "-110.3607",
+      "latitude" : "31.5273",
+      "ds" : "GL",
+      "city" : "Fort Huachuaca",
+      "country" : "United States",
+      "ip" : "6.131.144.10"
+    }
+  }
+}
+     */
     @Override
     public void run(String... args) throws Exception {
         RestTemplate restTemplate = new RestTemplate();
-        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-        log.info("\n\n VIEW QUOTE = " + quote.getValue().getQuote() + "\n\n");
+        StateInfo stateInfo = restTemplate.getForObject("http://geo.groupkt.com/ip/6.131.144.10/json", Quote.class);
+        log.info("\n\n VIEW State Info = " + stateInfo.toString() + "\n\n");
     }
 }
